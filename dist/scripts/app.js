60d44c4b0a8ff01ab3a1d70d88da19ea521eb820
Joseph Hakoopian <joseph.hakoopian@gmail.com>
2016-12-28 17:55:34 -0500

    add controllers

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -9,14 +9,16 @@
         $stateProvider
             .state('landing', {
                 url: '/',
+                controller: 'LandingCtrl as landing',
                 templateUrl: '/templates/landing.html'
             })
             .state('collection', {
-                url: '/',
+                url: '/collection',
+                controller: 'CollectionCtrl as collection',
                 templateUrl: '/templates/collection.html'
             })
             .state('album', {
-                url: '/',
+                url: '/album',
                 templateUrl: '/templates/album.html'
             });
         }
